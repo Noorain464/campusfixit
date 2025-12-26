1794a774d9a875a012a5b2f3b041d1c206cb838a
Syeda Noorain <syedanoorain@Syedas-MacBook-Air.local>
2025-12-26 23:42:31 +0530

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,41 @@
-import { Redirect } from "expo-router";
+import { useRouter } from "expo-router";
 import { useEffect, useState } from "react";
 import { getToken } from "../utils/api";
-import '../global.css'
+import { View, ActivityIndicator } from "react-native";
+import '../global.css';
 
 export default function Index() {
+  const router = useRouter(); // Initialize the router
   const [loading, setLoading] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const checkAuth = async () => {
-      const token = await getToken();
-      setIsLoggedIn(!!token);
-      setLoading(false);
+      try {
+        const token = await getToken(); 
+        
+        if (token) {
+          router.replace("/(tabs)");
+        } else {
+          router.replace("/login");
+        }
+      } catch (error) {
+        console.error("Auth check failed", error);
+        router.replace("/login");
+      } finally {
+        setLoading(false);
+      }
     };
 
     checkAuth();
   }, []);
 
-  if (loading) return null;
+  if (loading) {
+    return (
+      <View className="flex-1 justify-center items-center bg-white">
+        <ActivityIndicator size="large" color="#2563eb" />
+      </View>
+    );
+  }
 
-  return isLoggedIn ? (
-    <Redirect href="/(tabs)" />
-  ) : (
-    <Redirect href="/login" />
-  );
+  return null; 
 }
